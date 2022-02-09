717361faca7d745b195b929ca4c0ab42e3ed4b5a
Amanze Ogbonna <AmanzeIT@outlook.com>
2022-02-09 16:11:24 +0000

    loginpage

--- cypress/integration/rumpole_ui/page-objects/login-page.js
+++ cypress/integration/rumpole_ui/page-objects/login-page.js
@@ -1,6 +1,6 @@
 class LoginPage{
-
-
+  //loginUrl="https://as-web-rumpole-qa.azurewebsites.net/case-search"
+  //title="Find a case"
   loginUrl="https://phptravels.com/demo/"
   title="Demo Script Test drive - PHPTRAVELS"
 }
